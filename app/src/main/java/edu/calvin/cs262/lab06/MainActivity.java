e98b079f1c32dd40c65e6f3983cfcd3a827771dd
Zach-Wibbenmeyer <zdw3@students.calvin.edu>
2016-10-14 16:21:28 -0400

    Initial commit

---
+++ app/src/main/java/edu/calvin/cs262/lab06/MainActivity.java
@@ -0,0 +1,232 @@
+package edu.calvin.cs262.lab06;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Reads openweathermap's RESTful API for weather forecasts.
+ * The code is based on Deitel's WeatherViewer (Chapter 17), simplified based on Murach's NewsReader (Chapter 10).
+ * <p>
+ * for CS 262, lab 6
+ *
+ * @author kvlinden
+ * @version summer, 2016
+ * Completed by: Zach Wibbenmeyer
+ * Date: October 14, 2016
+ *
+ * Lab06 Answers to Questions:
+ * 1. If the user enters an invalid city, the app responds with a toast that says "failed to connect to service"
+ * 2. The API key is a key used to connect to the city's API to display the weather
+ * 3. The JSON looks similar to this:
+ * {"city":{"id":1851632,"name":"Shuzenji",
+"coord":{"lon":138.933334,"lat":34.966671},
+"country":"JP",
+"cod":"200",
+"message":0.0045,
+"cnt":38,
+"list":[{
+"dt":1406106000,
+"main":{
+"temp":298.77,
+"temp_min":298.77,
+"temp_max":298.774,
+"pressure":1005.93,
+"sea_level":1018.18,
+"grnd_level":1005.93,
+"humidity":87
+"temp_kf":0.26},
+"weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],
+"clouds":{"all":88},
+"wind":{"speed":5.71,"deg":229.501},
+"sys":{"pod":"d"},
+"dt_txt":"2014-07-23 09:00:00"}
+]}
+ * 4. It connects to the weather service and prints out the data in a readable format
+ * 5. The weather class is designed to get the weather data from openweathermap.org asynchronously
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private EditText cityText;
+    private Button fetchButton;
+
+    private List<Weather> weatherList = new ArrayList<>();
+    private ListView itemsListView;
+    private NumberFormat numberFormat = NumberFormat.getInstance();
+
+    /* This formater can be used as follows to format temperatures for display.
+     *     numberFormat.format(SOME_DOUBLE_VALUE)
+     */
+    //private NumberFormat numberFormat = NumberFormat.getInstance();
+
+    private static String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cityText = (EditText) findViewById(R.id.cityText);
+        fetchButton = (Button) findViewById(R.id.fetchButton);
+        itemsListView = (ListView) findViewById(R.id.weatherListView);
+
+        // See comments on this formatter above.
+        //numberFormat.setMaximumFractionDigits(0);
+
+        fetchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismissKeyboard(cityText);
+                new GetWeatherTask().execute(createURL(cityText.getText().toString()));
+            }
+        });
+    }
+
+    /**
+     * Formats a URL for the webservice specified in the string resources.
+     *
+     * @param city the target city
+     * @return URL formatted for openweathermap.com
+     */
+    private URL createURL(String city) {
+        try {
+            String urlString = getString(R.string.web_service_url) +
+                    URLEncoder.encode(city, "UTF-8") +
+                    "&units=" + getString(R.string.openweather_units) +
+                    "&cnt=" + getString(R.string.openweather_count) +
+                    "&APPID=" + getString(R.string.openweather_api_key);
+            return new URL(urlString);
+        } catch (Exception e) {
+            Toast.makeText(this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+        }
+
+        return null;
+    }
+
+    /**
+     * Deitel's method for programmatically dismissing the keyboard.
+     *
+     * @param view the TextView currently being edited
+     */
+    private void dismissKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * Inner class for GETing the current weather data from openweathermap.org asynchronously
+     */
+    private class GetWeatherTask extends AsyncTask<URL, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(URL... params) {
+            HttpURLConnection connection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                connection = (HttpURLConnection) params[0].openConnection();
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+                    return new JSONObject(result.toString());
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                connection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject weather) {
+            if (weather != null) {
+                //Log.d(TAG, weather.toString());
+                convertJSONtoArrayList(weather);
+                MainActivity.this.updateDisplay();
+            } else {
+                Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * Converts the JSON weather forecast data to an arraylist suitable for a listview adapter
+     *
+     * @param forecast
+     */
+    private void convertJSONtoArrayList(JSONObject forecast) {
+        weatherList.clear(); // clear old weather data
+        try {
+            JSONArray list = forecast.getJSONArray("list");
+            for (int i = 0; i < list.length(); i++) {
+                JSONObject day = list.getJSONObject(i);
+                JSONObject temperatures = day.getJSONObject("temp");
+                JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
+                weatherList.add(new Weather(
+                        day.getLong("dt"),
+                        temperatures.getDouble("min"),
+                        temperatures.getDouble("max"),
+                        weather.getString("description")));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Refresh the weather data on the forecast ListView through a simple adapter
+     */
+    private void updateDisplay() {
+        if (weatherList == null) {
+            Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+        }
+        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+        for (Weather item : weatherList) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("day", item.getDay());
+            map.put("description", item.getSummary());
+            map.put("min", numberFormat.format(item.getMin()));
+            map.put("max", numberFormat.format(item.getMax()));
+            data.add(map);
+        }
+
+        int resource = R.layout.weather_item;
+        String[] from = {"day", "description", "min", "max"};
+        int[] to = {R.id.dayTextView, R.id.summaryTextView, R.id.minTextView, R.id.maxTextView};
+
+        SimpleAdapter adapter = new SimpleAdapter(this, data, resource, from, to);
+        itemsListView.setAdapter(adapter);
+    }
+
+}
